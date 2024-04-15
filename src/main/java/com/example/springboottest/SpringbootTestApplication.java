10ecf3f8619af417323070f4590b968150016f03
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2024-04-15 10:20:45 +0200

    Log

--- src/main/java/com/example/springboottest/SpringbootTestApplication.java
+++ src/main/java/com/example/springboottest/SpringbootTestApplication.java
@@ -1,20 +1,31 @@
 package com.example.springboottest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableScheduling
+@Slf4j
 @RestController
 public class SpringbootTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootTestApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@Scheduled(fixedRate = 60000)
+	public void printHelloWorld() {
+		log.info("Hey! Let me do my job!");
 	}
 }
