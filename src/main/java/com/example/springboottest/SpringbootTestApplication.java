40429615c6d5a1e8bdb69fed77be28b258de12d6
Giampiero Porfiri <134400237+GiampieroPorfiri@users.noreply.github.com>
2024-04-15 10:13:03 +0200

    First commit

---
+++ src/main/java/com/example/springboottest/SpringbootTestApplication.java
@@ -0,0 +1,20 @@
+package com.example.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
+public class SpringbootTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
